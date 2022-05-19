c198d8af075fc77afc6318b9e0ed2404eb5d68c9
Andriy Andrunevchyn <diykorey@gmail.com>
2022-05-19 16:11:01 +0300

    init (#1)
    * mui template init
    
    * mui template init
    
    * init ui
    
    * configured reactrouter
    
    Co-authored-by: Myroslava Andrunevchyn <mhnatyuk@gmail.com>

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import OctoSearch from "./OctoSearch";
+import {Grid} from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
+import {ThemeProvider} from '@mui/material/styles';
+import theme from './theme';
+import MainMenuBar from "./MainMenuBar";
+import {Routes, Route, Outlet, Link} from "react-router-dom";
+
+const DocumentPage = React.lazy(() => import("./module/document/DocumentPage"));
+const ProfilePage = React.lazy(() => import("./module/profile/ProfilePage"));
+
+
+export default function App() {
+    return (
+        <ThemeProvider theme={theme}>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline/>
+            <MainMenuBar/>
+
+            <Routes>
+                <Route path="/">
+                    <Route index element={<Home/>}/>
+                    <Route
+                        path="about"
+                        element={
+                            <React.Suspense fallback={<>...</>}>
+                                <ProfilePage/>
+                            </React.Suspense>
+                        }
+                    />
+                    <Route
+                        path="dashboard/*"
+                        element={
+                            <React.Suspense fallback={<>...</>}>
+                                <DocumentPage/>
+                            </React.Suspense>
+                        }
+                    />
+                    <Route path="*" element={<NoMatch/>}/>
+                </Route>
+            </Routes>
+        </ThemeProvider>
+    );
+}
+
+function Home() {
+    return (
+        <Grid
+            container
+            spacing={0}
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            style={{minHeight: '70vh'}}
+        >
+
+            <Grid item xs={3}>
+                <OctoSearch/>
+            </Grid>
+
+        </Grid>
+    );
+}
+
+function NoMatch() {
+    return (
+        <div>
+            <h2>Nothing to see here!</h2>
+            <p>
+                <Link to="/">Go to the home page</Link>
+            </p>
+        </div>
+    );
+}
